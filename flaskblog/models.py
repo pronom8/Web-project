9bcb19527291f61f44cfce7220bdae10b65a427c
pronom8 <139699276+pronom8@users.noreply.github.com>
2024-04-26 11:42:03 +0300

    Created a new Comments class
    linked it with the Post class

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -45,6 +45,23 @@ class Post(db.Model):
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    comments = db.relationship('Comments', backref='parent', lazy=True)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+
+
+
+class Comments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+    
 
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')"
