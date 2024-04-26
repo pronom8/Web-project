f091c94c9c6c44aa4848e0501beb29caba525115
pronom8 <139699276+pronom8@users.noreply.github.com>
2024-04-26 12:51:17 +0300

    Imported comments from models

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -5,7 +5,7 @@ from flask import render_template, url_for, flash, redirect, request, abort
 from flaskblog import app, db, bcrypt, mail
 from flaskblog.forms import (RegistrationForm, LoginForm, UpdateAccountForm,
                               PostForm, RequestResetForm, ResetPasswordForm, TopicForm)
-from flaskblog.models import User, Post, Topic, TopicPosts
+from flaskblog.models import User, Post, Topic, TopicPosts, Comments
 from flask_login import login_user, current_user, logout_user, login_required
 from flask_mail import Message
 
