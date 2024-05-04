c3df6794ef70bb6c3066213b2cf3e84e1086fd8b
pronom8 <139699276+pronom8@users.noreply.github.com>
2024-05-04 21:33:55 +0300

    Updatted to match modern layout

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -4,10 +4,17 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from os import getenv
+import psycopg2
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
