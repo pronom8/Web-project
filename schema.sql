87033d740d9acc1464802e626db6800dbef88a15
pronom8 <139699276+pronom8@users.noreply.github.com>
2024-05-04 20:48:24 +0300

    Removed legend table and added all necessery tables for private area +
    made a new table for topic_post_comments

--- schema.sql
+++ schema.sql
@@ -1,10 +1,11 @@
-CREATE TABLE user (
+CREATE TABLE "user" (
     id SERIAL PRIMARY KEY,
     username VARCHAR(20) UNIQUE NOT NULL,
     email VARCHAR(120) UNIQUE NOT NULL,
     image_file VARCHAR(20) NOT NULL DEFAULT 'default.jpg',
     password VARCHAR(60) NOT NULL
 );
+
 
 CREATE TABLE post (
     id SERIAL PRIMARY KEY,
@@ -32,6 +33,18 @@ CREATE TABLE topic_posts (
 );
 
 
+CREATE TABLE topic_post_comments (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    date_posted TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    content TEXT NOT NULL,
+    topic_post_id INTEGER REFERENCES topic_posts (id) ON DELETE CASCADE NOT NULL,
+    topic_id INTEGER REFERENCES topic (id) NOT NULL,
+    user_id INTEGER REFERENCES "user" (id) NOT NULL
+);
+
+
+
 CREATE TABLE comments (
     id SERIAL PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
@@ -41,10 +54,7 @@ CREATE TABLE comments (
     user_id INTEGER REFERENCES "user" (id) NOT NULL
 );
 
-CREATE TABLE legend (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(100) NOT NULL
-);
+
 
 
 CREATE TABLE private_area (
@@ -52,5 +62,31 @@ CREATE TABLE private_area (
     title VARCHAR(100) NOT NULL,
     date_posted TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     content TEXT NOT NULL,
+    user_id INTEGER REFERENCES "user" (id) NOT NULL
+);
+
+CREATE TABLE private_area_user (
+    id SERIAL PRIMARY KEY,
+    private_area_id INTEGER REFERENCES private_area(id) NOT NULL,
+    user_id INTEGER REFERENCES "user"(id),
+    UNIQUE(private_area_id, user_id)  
+);
+
+CREATE TABLE private_area_posts (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    date_posted TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    content TEXT NOT NULL,
+    private_area_id INTEGER REFERENCES private_area (id) NOT NULL,
+    user_id INTEGER REFERENCES "user" (id) NOT NULL
+);
+
+CREATE TABLE private_area_post_comments (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    date_posted TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    content TEXT NOT NULL,
+    private_area_post_id INTEGER REFERENCES private_posts (id) ON DELETE CASCADE NOT NULL,
+    private_area_id INTEGER REFERENCES private_area (id) NOT NULL,
     user_id INTEGER REFERENCES "user" (id) NOT NULL
 );
